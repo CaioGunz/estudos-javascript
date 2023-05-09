cb6e78f28b3b915bdc74603f6882e88381522b0e
CaioGunz <caiobarbosa2404@hotmail.com>
2023-05-09 16:55:57 -0300

    Definindo a voz com Web Speech

--- numero-secreto/app/sortearNumero.js
+++ numero-secreto/app/sortearNumero.js
@@ -1,1 +1,13 @@
-console.log("estou aqui")
+const menorValor = 1
+const maiorValor = 100
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML = menorValor
+
+const elementomMaiorValor = document.getElementById('maior-valor')
+elementomMaiorValor.innerHTML = maiorValor
