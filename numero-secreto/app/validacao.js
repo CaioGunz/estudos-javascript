a0d2e2100eac43362b078d4090842bd934e95db0
CaioGunz <caiobarbosa2404@hotmail.com>
2023-05-10 15:19:50 -0300

    Criando a lógica do jogo

---
+++ numero-secreto/app/validacao.js
@@ -0,0 +1,34 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor Inválido</div>'
+    }
+    if(numeroForMaiorouMenorQueOValorPermitido(numero)){
+        elementoChute.innerHTML += `
+            <div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
+    }
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+            <h2>Você Acertou!!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if(numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>-->
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>-->
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorouMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
