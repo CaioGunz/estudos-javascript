c3b62f784430a1d52ba5a6555de9bbfddaa8171f
CaioGunz <caiobarbosa2404@hotmail.com>
2023-05-12 14:44:23 -0300

    Aprendendo sobre: MAP, novo array modificado

--- alura-books/app/main.js
+++ alura-books/app/main.js
@@ -2,28 +2,12 @@ let livros = []
 const endPointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
 
-const elementeParaInserirLivros = document.getElementById('livros')
+
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endPointDaAPI)
     livros = await res.json()
-    exibirLivrosNaTela(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirLivrosNaTela(livrosComDesconto)
 }
 
-function exibirLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => (
-        elementeParaInserirLivros.innerHTML += `
-            <div class="livro">
-                <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-                <h2 class="livro__titulo">
-                ${livro.titulo}
-                </h2>
-                <p class="livro__descricao">${livro.autor}</p>
-                <p class="livro__preco" id="preco">${livro.preco}</p>
-                <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-                </div>
-            </div>
-        `
-    ))
-}
