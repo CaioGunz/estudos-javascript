f0860d2cd49a50a9aa30d0c3a219958574742493
CaioGunz <caiobarbosa2404@hotmail.com>
2023-05-12 13:48:48 -0300

    Inclusão do projeto Alura Books

--- pagina-de-cadastro-dados/script.js
+++ pagina-de-cadastro-dados/script.js
@@ -1,14 +1,29 @@
-async function buscaEndereco(){
+async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = "";
     try {
-        var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCepConvertida = await consultaCep.json();
         if(consultaCepConvertida.erro){
             throw Error('CEP não existente!')
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+        
+        cidade.value = consultaCepConvertida.localidade;
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
+        bairro.value = consultaCepConvertida.bairro;
+
         console.log(consultaCepConvertida);
+        return consultaCepConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP Inválido. Tente Novamente</p>`
         console.log(erro);
     } 
 }
 
-buscaEndereco();
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
