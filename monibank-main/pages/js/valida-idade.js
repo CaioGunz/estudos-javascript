acf16be7085596718ffd8dc334c0927f973c52a1
CaioGunz <caiobarbosa2404@hotmail.com>
2023-05-18 15:46:46 -0300

    Comunicando ao Usuário

--- monibank-main/pages/js/valida-idade.js
+++ monibank-main/pages/js/valida-idade.js
@@ -1,6 +1,8 @@
 export default function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
-    validaIdade(dataNascimento);
+    if (!validaIdades(dataNascimento)) {
+        campo.setCustomValidity('O usuário não é maior de idade');
+    }
 
     
 }
@@ -9,5 +11,5 @@ function validaIdade(data) {
     const dataAtual = new Date();
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
 
-    return dataAtual >= dataMais18
+    return dataAtual >= dataMais18;
 }
